813e55003a09ba70b960841c1d7f545f18c0363c
nkulway <nkulway@gmail.com>
2025-01-18 18:23:45 -0500

    abstract out logic into task list component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,57 +1,14 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TaskService } from './services/task.service';
-import { FormsModule } from '@angular/forms';
+import { TaskListComponent } from './components/task-list/task-list.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule, FormsModule],
+  imports: [CommonModule, RouterOutlet, TaskListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'Angular Application with a Rails Backend';
-  tasks: any[] = [];
-  newTask = { title: '', name: '', completed: false, description: '' };
-  editingTask: any = null
-
-  constructor(private taskService: TaskService) {}
-
-  fetchData() {
-    this.taskService.getTasks().subscribe((data: any) => {
-      this.tasks = data;
-      console.log('Here is my data => ', data);
-    });
-  }
-
-  addTask() {
-    this.taskService.createTask(this.newTask).subscribe((data) => {
-      console.log('task added -> ', data);
-      this.fetchData();
-      this.newTask = { title: '', name: '', completed: false, description: '' };
-    });
-  }
-
-  startEditTask(task: any) {
-    this.editingTask = {...task}
-  }
-
-  updateTask() {
-    this.taskService.updateTask(this.editingTask.id, this.editingTask).subscribe((data) => {
-      this.fetchData()
-      this.editingTask = null
-    })
-  }
-
-  cancelEdit() {
-    this.editingTask = null
-  }
-
-  deleteTask(taskId: number) {
-    this.taskService.deleteTask(taskId).subscribe((data) => {
-      console.log('task deleted -> ', taskId);
-      this.tasks = this.tasks.filter((task) => task.id !== taskId);
-    });
-  }
 }
--- src/app/components/task-list/task-list.component.ts
+++ src/app/components/task-list/task-list.component.ts
@@ -1,20 +1,63 @@
 import { Component } from '@angular/core';
 import { TaskService } from '../../services/task.service';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-task-list',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.scss',
 })
 export class TaskListComponent {
   tasks: any[] = [];
+  newTask = { title: '', name: '', completed: false, description: '' };
+  editingTask: any = null;
 
   constructor(private taskService: TaskService) {}
 
   ngOnInit(): void {
     this.taskService.getTasks().subscribe((data) => {
       this.tasks = data;
+    });
+  }
+
+  fetchData() {
+    this.taskService.getTasks().subscribe((data: any) => {
+      this.tasks = data;
+      console.log('Here is my data => ', data);
+    });
+  }
+
+  addTask() {
+    this.taskService.createTask(this.newTask).subscribe((data) => {
+      console.log('task added -> ', data);
+      this.fetchData();
+      this.newTask = { title: '', name: '', completed: false, description: '' };
+    });
+  }
+
+  startEditTask(task: any) {
+    this.editingTask = { ...task };
+  }
+
+  updateTask() {
+    this.taskService
+      .updateTask(this.editingTask.id, this.editingTask)
+      .subscribe((data) => {
+        this.fetchData();
+        this.editingTask = null;
+      });
+  }
+
+  cancelEdit() {
+    this.editingTask = null;
+  }
+
+  deleteTask(taskId: number) {
+    this.taskService.deleteTask(taskId).subscribe((data) => {
+      console.log('task deleted -> ', taskId);
+      this.tasks = this.tasks.filter((task) => task.id !== taskId);
     });
   }
 }
